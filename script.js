5f3d90be92b6b78eb7cb7b74f62a9aa7e9e479e6
breno romeiro <bsromeiro@gmail.com>
2020-04-28 16:29:12 -0300

    add empty field alert

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ function calculate() {
     let number = document.getElementById('number')
     let num = Number(number.value)
     let res = document.getElementById('res')
-    
-    let inc = 1
-
     res.innerHTML = ''
-    for( let n = num ; inc <= 10 ; inc++ ) {
-        let item = document.createElement('option')
-        item.text = `${n} x ${inc} = ${n*inc}`
-        res.appendChild(item)
+    
+    if(number.value.length == 0) {
+        alert('Insert a number')
+    } else {
+        let inc = 1
+        for( let n = num ; inc <= 10 ; inc++ ) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${inc} = ${n*inc}`
+            item.value = `res${inc}`
+            res.appendChild(item)
+        }
     }
 }
